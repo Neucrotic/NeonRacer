5218220f66b044ac6fe277fc12d78b6e4c51f888
tommas solarino <tomster954@gmail.com>
2015-05-13 10:56:52 +1000

    made changes to the terrain

--- Assets/Mine/Scripts/TerrainAMovement.cs
+++ Assets/Mine/Scripts/TerrainAMovement.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class TerrainAMovement : MonoBehaviour {
 
+    public GameObject p_player;
     private bool m_moveTerrainB = true;
+    private float m_speed = 70;
+
 	// Use this for initialization
 	void Start () 
     {
@@ -13,11 +16,14 @@ public class TerrainAMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        float speed = 70;
         int terrainLength = 500;
 
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
+        m_speed = p_player.GetComponent<Player>().p_movement.y;
 
+        //Move this terrain towards the player.
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - m_speed * Time.deltaTime);
+
+        //this move the other terrain to the back of this one
         if (this.transform.position.z <= -250 && m_moveTerrainB)
         {
             GameObject b = GameObject.FindGameObjectWithTag("TerrainB");
--- Assets/Mine/Scripts/TerrainBMovement.cs
+++ Assets/Mine/Scripts/TerrainBMovement.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class TerrainBMovement : MonoBehaviour {
 
 	// Use this for initialization
+    public GameObject p_player;
     private bool m_moveTerrainA = true;
+    private float m_speed = 70;
 	void Start () {
 	
 	}
@@ -12,11 +14,14 @@ public class TerrainBMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        float speed = 70;
         float terrainLength = 500f;
 
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - speed * Time.deltaTime);
+        m_speed = p_player.GetComponent<Player>().p_movement.y;
 
+        //Move this terrain towards the player.
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - m_speed * Time.deltaTime);
+
+        //this move the other terrain to the back of this one
         if (this.transform.position.z <= -250 && m_moveTerrainA)
         {
             GameObject a = GameObject.FindGameObjectWithTag("TerrainA");
